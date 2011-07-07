f5546b2d134a5c9893728c625b2b631725b678f3
Ma Can <ml.macana@gmail.com>
2011-07-08 01:06:00 +0800

    Extend unit test's latency plot script: more ranges and logscale.

--- bin/unit_latency_distribution.sh
+++ bin/unit_latency_distribution.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 if [ "x$1" == "x" ]; then
-    echo "Usage: $0 client_lat-logfile"
+    echo "Usage: $0 client_lat-logfile XRANGE_END"
     exit
+fi
+
+if [ "x$2" == "x" ]; then
+    SETRANGE=""
+elif [ "x$2" == "xignore" ]; then
+    SETRANGE=""
+else
+    SETRANGE="set xrange [0:$2]"
+fi
+
+if [ "x$3" == "xlog" ]; then
+    SETXLOGSCALE="set logscale x 2"
+else
+    SETXLOGSCALE=""
 fi
 
 cat $1 | sed -n '/real test/, $ {/<=/ {p}}' | awk '{print $2, $4, $6, $7}' > $1.uld
@@ -19,6 +33,8 @@ set output '$1.png'
 set auto x
 set auto y
 
+$SETRANGE
+$SETXLOGSCALE
 set xlabel 'Latency (ms)'
 set ylabel 'Percentage (%)'
 set ytics nomirror
